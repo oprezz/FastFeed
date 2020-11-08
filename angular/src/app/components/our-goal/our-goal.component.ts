126df413d1b1762bfb6c25d40e44b95049daf021
tszab <szabolcs.torma96@gmail.com>
2020-11-08 14:33:47 +0100

    leftout components added to git

---
+++ angular/src/app/components/our-goal/our-goal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-our-goal',
+  templateUrl: './our-goal.component.html',
+  styleUrls: ['./our-goal.component.scss']
+})
+export class OurGoalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ angular/src/app/components/weather/weather.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Weather } from '../../classes/weather';
+
+@Component({
+  selector: 'app-weather',
+  templateUrl: './weather.component.html',
+  styleUrls: ['./weather.component.scss']
+})
+export class WeatherComponent implements OnInit {
+
+  weather: Weather = {
+    city: 'Los Angeles',
+    conditions: 'Sunny',
+    temperature: 70,
+    icon: ''
+  };
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
