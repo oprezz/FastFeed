bed6638056e76d701518146645823d555d7366bc
tszab <szabolcs.torma96@gmail.com>
2020-11-10 17:12:30 +0100

    CalendarAPI added, importing a calendar is not possible yet

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,7 +1,14 @@
+// Modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { ScheduleModule } from '@syncfusion/ej2-angular-schedule';
+// Services
+import { WeatherAPIService } from './services/weather-api.service';
+import { DayService, AgendaService } from '@syncfusion/ej2-angular-schedule';
+import { View } from '@syncfusion/ej2-schedule';
+// Components
 import { AppComponent } from './app.component';
 import { MenuComponent } from './components/menu/menu.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -9,9 +16,6 @@ import { FooterComponent } from './components/footer/footer.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { OurGoalComponent } from './components/our-goal/our-goal.component';
 import { WeatherComponent } from './components/weather/weather.component';
-
-import { AppRoutingModule } from './app-routing/app-routing.module';
-import { WeatherAPIService } from './services/weather-api.service';
 import { CalendarComponent } from './components/calendar/calendar.component';
 import { FoodDetailsComponent } from './components/food-details/food-details.component';
 
@@ -30,10 +34,16 @@ import { FoodDetailsComponent } from './components/food-details/food-details.com
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    // Calendar modules
+    ScheduleModule
   ],
   providers: [
-    WeatherAPIService
+    // Weather API services
+    WeatherAPIService,
+    // Calendar services
+    DayService,
+    AgendaService
   ],
   bootstrap: [AppComponent]
 })
--- angular/src/app/components/calendar/calendar.component.ts
+++ angular/src/app/components/calendar/calendar.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+
+import { View } from '@syncfusion/ej2-schedule';
 
 @Component({
   selector: 'app-calendar',
@@ -6,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calendar.component.scss']
 })
 export class CalendarComponent implements OnInit {
-
+  public todaysDate = new Date();
+  public calendarView: View = 'Day';
   constructor() { }
 
   ngOnInit(): void {
