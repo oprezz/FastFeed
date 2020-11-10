a825b78810ce917c663c29552ed2799505863393
oprezz <antal.marci@gmail.com>
2020-11-10 16:24:23 +0100

    merge clean up

---
+++ nodejs_express/bin/db.js
@@ -0,0 +1,52 @@
+const mongoClient = require('mongodb').MongoClient;
+const mongoDbUrl = "mongodb+srv://admin:fastfeed@cluster0.dvpo1.mongodb.net/<dbname>?retryWrites=true&w=majority";
+let mongodb;
+ // The database to use
+const dbName = "FastFeedDB";
+                    
+// async function run() { //to be deleted
+//     try {
+//             await client.connect();
+//             console.log("Connected correctly to server");
+//             db = client.db(dbName);
+
+//             // Use the collection "users"
+//             const usercol = db.collection("users");
+
+//             // Insert a single document, wait for promise so we can read it back
+//             const p = await usercol.insertOne(personDocument);
+//             // Find one document
+//             const myDoc = await usercol.findOne();
+//             // Print to the console
+//             console.log(myDoc);
+
+//         } catch (err) {
+//             console.log(err.stack);
+//         }
+
+//         finally {
+//         await client.close();
+//     }
+// }
+
+function connect(callback){
+    mongoClient.connect(mongoDbUrl, (err, db) => {
+        if (err) return console.log(err);
+        mongodb = db.db(dbName);
+        callback();
+    });
+}
+
+function get(){
+    return mongodb;
+}
+
+function close(){
+    mongodb.close();
+}
+
+module.exports = {
+    connect,
+    get,
+    close
+};
