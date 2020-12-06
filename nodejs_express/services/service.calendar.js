b6473ed2e380fc1664c82852e6e8b54b2f112aa4
tszab <szabolcs.torma96@gmail.com>
2020-12-06 14:34:03 +0100

    Lunch appointment created in backed for recommendation

--- nodejs_express/services/service.calendar.js
+++ nodejs_express/services/service.calendar.js
@@ -1,8 +1,7 @@
-
+let uid_gen = require('uuid');
 const CalendarModel = require("../models/model.calendar");
 let Validator = require('fastest-validator');
 var db = require('../bin/db.js')
-
 
 let calendars = {};
 let calendarCounter = 0;
@@ -56,12 +55,19 @@ class CalendarService
 
     static checkNSetIfDiffIsOneHour(currAppointmentEnd, nextAppointmentStart, lunchTimeApp)
     {
-        let diff = currAppointmentEnd - nextAppointmentStart;
+        let diff = nextAppointmentStart - currAppointmentEnd;
+        console.log("ADIFF")
+        console.log(diff)
         let diffInHours = this.convertToHours(diff);
+        console.log("CHECKES")
+        console.log(diffInHours)
         if(diffInHours.hours >= 1 || (diffInHours.hours === 0 && diffInHours.minutes === 59)){
-            lunchTimeApp.StartTime = currAppointmentEnd;
+            lunchTimeApp.StartTime = currAppointmentEnd.toISOString();
             lunchTimeApp.EndTime = currAppointmentEnd;
-            lunchTimeApp.EndTime.setHours(startLunch.getHours() + 1);
+            lunchTimeApp.EndTime.setHours(currAppointmentEnd.getHours() + 1);
+            lunchTimeApp.EndTime = lunchTimeApp.EndTime.toISOString()
+            console.log("INDIFF FUNC")
+            console.log(lunchTimeApp)
             return true;
         }
         return false;
@@ -69,12 +75,13 @@ class CalendarService
 
     static checkNSetIfDiffIsHalfHour(currAppointmentEnd, nextAppointmentStart, lunchTimeApp)
     {
-        let diff = currAppointmentEnd - nextAppointmentStart;
+        let diff = nextAppointmentStart - currAppointmentEnd;
         let diffInHours = this.convertToHours(diff);
         if(diffInHours.hours >= 1 || (diffInHours.hours === 0 && diffInHours.minutes >= 29)){
-            lunchTimeApp.StartTime = currAppointmentEnd;
+            lunchTimeApp.StartTime = currAppointmentEnd.toISOString();
             lunchTimeApp.EndTime = currAppointmentEnd;
-            lunchTimeApp.EndTime.setHours(startLunch.getHours() + 1);
+            lunchTimeApp.EndTime.setHours(currAppointmentEnd.getHours() + 1);
+            lunchTimeApp.EndTime = lunchTimeApp.EndTime.toISOString()
             return true;
         }
         return false;
@@ -83,27 +90,49 @@ class CalendarService
     static searchForWholeHour(shortBlockingApps, startLunch, endLunch, lunchTimeApp)
     {
         let lunchTimeFound = false;
-        for(let appIdx = 0; appIdx < (shortBlockingApps.length - 1); appIdx++){
-            let currAppointment = shortBlockingApps[appIdx];
-            let nextAppointment = shortBlockingApps[appIdx + 1];
-
-            if(appIdx === 0){
-                if(currAppointment.StartTime > startLunch){
-                    lunchTimeFound = this.checkNSetIfDiffIsOneHour(startLunch, currAppointment.StartTime, lunchTimeApp);
-                    if (lunchTimeFound)
-                        return lunchTimeApp;
-                }
+        if (shortBlockingApps.length === 1)
+        {
+            let currAppointment = shortBlockingApps[0];
+            if (new Date(currAppointment.StartTime) > startLunch){
+                lunchTimeFound = this.checkNSetIfDiffIsOneHour(startLunch, new Date(currAppointment.StartTime), lunchTimeApp);
+                if (lunchTimeFound)
+                    return lunchTimeApp;
             }
+            else if (new Date(currAppointment.EndTime) < endLunch){
+                lunchTimeFound = this.checkNSetIfDiffIsOneHour(new Date(currAppointment.EndTime), endLunch, lunchTimeApp);
+                if (lunchTimeFound)
+                    return lunchTimeApp;
+            }
+        }
+        else{
+            for(let appIdx = 0; appIdx < (shortBlockingApps.length - 1); appIdx++){
+                console.log(appIdx)
+                let currAppointment = shortBlockingApps[appIdx];
+                let nextAppointment = shortBlockingApps[appIdx + 1];
 
-            lunchTimeFound = this.checkNSetIfDiffIsOneHour(currAppointment.EndTime, nextAppointment.StartTime, lunchTimeApp);
-            if(lunchTimeFound)
-                return lunchTimeApp;
+                if(appIdx === 0){
+                    if(new Date(currAppointment.StartTime) > startLunch){
+                        lunchTimeFound = this.checkNSetIfDiffIsOneHour(startLunch, new Date(currAppointment.StartTime), lunchTimeApp);
+                        if (lunchTimeFound)
+                            return lunchTimeApp;
+                    }
+                    else if (new Date(currAppointment.EndTime) < endLunch){
+                        lunchTimeFound = this.checkNSetIfDiffIsOneHour(new Date(currAppointment.EndTime), endLunch, lunchTimeApp);
+                        if (lunchTimeFound)
+                            return lunchTimeApp;
+                    }
+                }
 
-            if (appIdx === (shortBlockingApps.length - 2)) {
-                if (nextAppointment.EndTime < endLunch) {
-                    lunchTimeFound = this.checkNSetIfDiffIsOneHour(nextAppointment.EndTime, endLunch, lunchTimeApp);
-                    if (lunchTimeFound)
-                        return lunchTimeApp;
+                lunchTimeFound = this.checkNSetIfDiffIsOneHour(new Date(currAppointment.EndTime), new Date(nextAppointment.StartTime), lunchTimeApp);
+                if(lunchTimeFound)
+                    return lunchTimeApp;
+
+                if (appIdx === (shortBlockingApps.length - 2)) {
+                    if (new Date(nextAppointment.EndTime) < endLunch) {
+                        lunchTimeFound = this.checkNSetIfDiffIsOneHour(new Date(nextAppointment.EndTime), endLunch, lunchTimeApp);
+                        if (lunchTimeFound)
+                            return lunchTimeApp;
+                    }
                 }
             }
         }
@@ -118,20 +147,20 @@ class CalendarService
             let nextAppointment = shortBlockingApps[appIdx + 1];
 
             if(appIdx === 0){
-                if(currAppointment.StartTime > startLunch){
-                    lunchTimeFound = this.checkNSetIfDiffIsHalfHour(startLunch, currAppointment.StartTime, lunchTimeApp);
+                if(new Date(currAppointment.StartTime) > startLunch){
+                    lunchTimeFound = this.checkNSetIfDiffIsHalfHour(startLunch, new Date(currAppointment.StartTime), lunchTimeApp);
                     if (lunchTimeFound)
                         return lunchTimeApp;
                 }
             }
 
-            lunchTimeFound = this.checkNSetIfDiffIsHalfHour(currAppointment.EndTime, nextAppointment.StartTime, lunchTimeApp);
+            lunchTimeFound = this.checkNSetIfDiffIsHalfHour(new Date(currAppointment.EndTime), new Date(nextAppointment.StartTime), lunchTimeApp);
             if(lunchTimeFound)
                 return lunchTimeApp;
 
             if (appIdx === (shortBlockingApps.length - 2)) {
-                if (nextAppointment.EndTime < endLunch) {
-                    lunchTimeFound = this.checkNSetIfDiffIsHalfHour(nextAppointment.EndTime, endLunch, lunchTimeApp);
+                if (new Date(nextAppointment.EndTime) < endLunch) {
+                    lunchTimeFound = this.checkNSetIfDiffIsHalfHour(new Date(nextAppointment.EndTime), endLunch, lunchTimeApp);
                     if (lunchTimeFound)
                         return lunchTimeApp;
                 }
@@ -143,8 +172,10 @@ class CalendarService
     static recommendLunch(calendar)
     {
         let todayDate = new Date();
+        todayDate.setDate(todayDate.getDate()+1);
         let tomorrowDate = new Date();
-        tomorrowDate.setDate(tomorrowDate.getDate() + 1);
+        tomorrowDate.setDate(tomorrowDate.getDate() + 2);
+
         let dailyEventsArray = calendar.EventArray.filter(appointment => {
             let startTime = new Date(appointment.StartTime);
             return ((todayDate.getFullYear() === startTime.getFullYear()) &&
@@ -152,60 +183,74 @@ class CalendarService
                     (todayDate.getDate() === startTime.getDate()));
         }).map(appointment => appointment);
 
+        dailyEventsArray.sort((a,b) => {
+            return new Date(b.StartTime) - new Date(a.StartTime);
+        });
+
         const startLunch = new Date(dailyEventsArray[0].StartTime);
         startLunch.setHours(11,0,0,1);
         const endLunch = new Date(dailyEventsArray[0].StartTime);
         endLunch.setHours(13,59,59,999);
 
+
         // Check if there is an appointment that overlaps the whole lunch break time
         let lunchOverlappingApps = dailyEventsArray.filter(appointment => {
-            return appointment.StartTime < startLunch && appointment.EndTime > endLunch;
+            return new Date(appointment.StartTime) < startLunch && new Date(appointment.EndTime) > endLunch;
         }).map(appointment => appointment);
         // If overlapping appointment exists, we wont recommend anything
         if (lunchOverlappingApps.length > 0){
-            return null;
+            return calendar;
         }
 
         // Check for appointments which start before 11 and finish in lunch break or start before 14 and finish
         // after lunch break
         let shortBlockingApps = dailyEventsArray.filter(appointment => {
-            return (endLunch > appointment.StartTime > startLunch) || (startLunch < appointment.EndTime < endLunch);
+            return (startLunch < new Date(appointment.StartTime) && new Date(appointment.StartTime) < endLunch)
+                || (startLunch < new Date(appointment.EndTime) && new Date(appointment.EndTime) < endLunch);
         }).map(appointment => appointment);
+
         // If there are no existing blockers, set lunch recommendation at 12-13
         if (shortBlockingApps.length === 0){
-            // Create appointment and set attributes
-
-            // Call for recommendation
-
+            let lunchTimeApp = JSON.parse(JSON.stringify(dailyEventsArray[0]));
+            lunchTimeApp.UID = uid_gen.v4();
+            lunchTimeApp.Id = 999;
+            lunchTimeApp.Subject = "Lunch";
+            lunchTimeApp.StartTime = new Date(lunchTimeApp.StartTime).setHours(12,0,0,0).toISOString();
+            lunchTimeApp.EndTime = new Date(lunchTimeApp.EndTime).setHours(13,0,0,0).toISOString();
+            calendar.EventArray.push(lunchTimeApp);
+            return calendar;
         }
         else{
             // Search for an empty hour
-            let lunchTimeFound = false;
-            let lunchTimeApp = shortBlockingApps[0];
-            lunchTimeApp.StartTime.setHours(0,0,0,0);
+            let lunchTimeApp = JSON.parse(JSON.stringify(shortBlockingApps[0]));
+            lunchTimeApp.UID = uid_gen.v4();
+            lunchTimeApp.Id = 999;
+            lunchTimeApp.Subject = "Lunch";
+            lunchTimeApp.StartTime = new Date(lunchTimeApp.StartTime);
+            lunchTimeApp.StartTime.setHours(1,0,0,0);
+            lunchTimeApp.StartTime = lunchTimeApp.StartTime.toISOString();
 
             lunchTimeApp = this.searchForWholeHour(shortBlockingApps, startLunch, endLunch, lunchTimeApp)
-            if(lunchTimeApp.StartTime.getHours() !== 0){
-                // Create appointment and set attributes
-
-                // Call for recommendation
+                if(new Date(lunchTimeApp.StartTime).getHours() !== 1){
+                calendar.EventArray.push(lunchTimeApp);
+                return calendar;
             }
 
             // If the prev search was unsuccessful, search for a half an hour
             lunchTimeApp = this.searchForWholeHour(shortBlockingApps, startLunch, endLunch, lunchTimeApp)
-            if(lunchTimeApp.StartTime.getHours() !== 0){
-                // Create appointment and set attributes
-
-                // Call for recommendation
+            if(new Date(lunchTimeApp.StartTime).getHours() !== 1){
+                calendar.EventArray.push(lunchTimeApp);
+                return calendar;
             }
         }
+        return calendar;
     }
 
     static async retrieve(ownerGuid)
     {
         console.log('Requested user by username:', ownerGuid);
         const calendarCol = await db.get().collection("calendars");
-        const calendar = await calendarCol.findOne({OwnerGuid: ownerGuid});
+        let calendar = await calendarCol.findOne({OwnerGuid: ownerGuid});
         if(calendar == null)
         {
             console.error('Unable to retrieve a user by (ownerGuid:'+ ownerGuid +')');
@@ -213,7 +258,7 @@ class CalendarService
         else
         {
             console.log('User (ownerGuid:'+ ownerGuid +') retrieved from DB!');
-            this.recommendLunch(calendar);
+            calendar = this.recommendLunch(calendar);
         }
         return calendar;
     }
